2a6197f6527a489f9e2d25912e3fb9034aa87b5b
godmeta <103444984+godmeta@users.noreply.github.com>
2022-08-06 11:27:58 +0800

    Create dmq.js

---
+++ gdb/dmq.js
@@ -0,0 +1,5 @@
+s='Do you believe that technologies will empower each of us, enabling us to make a better world?' 
+h=bnToB64(cyrb53(s)) 
+a={h:h,s:s} 
+JSON.stringify(a) 
+// "{\"h\":\"H1881U14XA==\",\"s\":\"Do you believe that technologies will empower each of us, enabling us to make a better world?\"}" 
