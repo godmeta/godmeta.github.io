a779ab28e18b904923c2eb0143e03da530778083
godmeta <103444984+godmeta@users.noreply.github.com>
2022-08-06 22:38:10 +0800

    Create dmq001.js

---
+++ gdb/dmq001.js
@@ -0,0 +1,5 @@
+s='https://github.com/godmeta/godmeta.github.io/blob/main/gdb/dmq001.md' 
+h=bnToB64(cyrb53(s)) 
+a={h:h,s:s} 
+JSON.stringify(a) 
+// "{\"h\":\"BzXdgpJGRw==\",\"s\":\"https://github.com/godmeta/godmeta.github.io/blob/main/gdb/dmq001.md\"}"
